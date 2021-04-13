04462d9f3b5b7924505ef206dee5d429e8bf90b6
w12zard <john.henson@gmail.com>
2021-04-12 17:25:05 -0700

    Added some try/except blocks to catch exceptions

--- valheim_backup.py
+++ valheim_backup.py
@@ -4,13 +4,15 @@ from os import listdir, mkdir, path
 from ftplib import FTP
 import json
 
-
 ## Edit and rename vhbuconfig-SAMPLE.json
 CONFIG_FILE = './vhbuconfig.json'
 
 #read config file and initialize
-with open(CONFIG_FILE) as file:
-    conf = json.load(file)
+try:
+    with open(CONFIG_FILE) as file:
+        conf = json.load(file)
+except:
+    print("Oopsy. Couldn't fine the config file.")
 
 #initialize vars with config file values
 host = conf["host"]
@@ -44,41 +46,50 @@ mkdir(vh_ftp_world_backup_fullpath)
 
 #actually copy
 print("Backing up local files from: " + vh_user_data_dir + " to: " + vh_backup_fullpath)
-copytree(vh_char_path, vh_char_backup_fullpath)
-copytree(vh_world_path, vh_world_backup_fullpath)
+try:
+    copytree(vh_char_path, vh_char_backup_fullpath)
+    copytree(vh_world_path, vh_world_backup_fullpath)
+except:
+    print("Couldn't copy local backup files. Check that the config file is correct")
 
 #Copy FTP server files from g-portal
-ftp = FTP()
-#connect
-print("Connecting to ftp server: " + host)
-ftp.connect(host, port)
-ftp.login(username, password)
-#change dir
-ftp.cwd(ftp_subdir)
+try:
+    ftp = FTP()
+    #connect
+    print("Connecting to ftp server: " + host)
+    ftp.connect(host, port)
+    ftp.login(username, password)
+    #change dir
+    ftp.cwd(ftp_subdir)
 
-#list files and put filenames in an array
-listings = []
-filenames = []
-ftp.retrlines('LIST', listings.append)
-for listing in listings:
-    f = listing.split(None, 8)
-    fname = f[8]
-    filenames.append(fname)
+    #list files and put filenames in an array
+    listings = []
+    filenames = []
+    ftp.retrlines('LIST', listings.append)
+    for listing in listings:
+        f = listing.split(None, 8)
+        fname = f[8]
+        filenames.append(fname)
 
-#find the files that match the server string
-print("Downloading all files matching substring " + ftp_substring)
-files_to_download = []
-for match in filenames:
-    if ftp_substring in match:
-        files_to_download.append(match)
+    #find the files that match the server string
+    print("Downloading all files matching substring " + ftp_substring)
+    files_to_download = []
+    for match in filenames:
+        if ftp_substring in match:
+            files_to_download.append(match)
 
-f = files_to_download[0] #replace with loop for all matching files
-for f in files_to_download:
-    local_filename = path.join(vh_ftp_world_backup_fullpath, f)
-    lf = open(local_filename, "wb")
-    ftp.retrbinary("RETR " + f, lf.write)
-    lf.close()
+    f = files_to_download[0] #replace with loop for all matching files
+    for f in files_to_download:
+        local_filename = path.join(vh_ftp_world_backup_fullpath, f)
+        lf = open(local_filename, "wb")
+        ftp.retrbinary("RETR " + f, lf.write)
+        lf.close()
 
-#close connection
-ftp.quit()
+    #close connection
+    ftp.quit()
+
+except:
+    print("FTP step failed. Check config file settings and try again.")
+
 print("All done.")
+    
